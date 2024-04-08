64220eafc2a38696956d40e1bc8455bebec55902
Anaple <67675386+Anaple@users.noreply.github.com>
2024-04-08 17:38:27 +0800

    2.0
    update rknn_api2.0

--- rknn_2/include/rknnPool.hpp
+++ rknn_2/include/rknnPool.hpp
@@ -1,386 +1,107 @@
-#ifndef _rknnPool_H
-#define _rknnPool_H
+#ifndef RKNNPOOL_H
+#define RKNNPOOL_H
 
-#include <queue>
+#include "ThreadPool.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "rkYolov5s.hpp"
+
 #include <vector>
 #include <iostream>
-#include "rga.h"
-#include "im2d.h"
-#include "RgaUtils.h"
-#include "rknn_api.h"
-#include "postprocess.h"
-#include "opencv2/core/core.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/imgproc.hpp"
-#include <sensor_msgs/msg/image.hpp>
-#include <msg_interfaces/msg/rgb_camera_obstacle.hpp>
-#include "ThreadPool.hpp"
-using cv::Mat;
-using std::queue;
-using std::vector;
+#include <mutex>
+#include <queue>
+#include <memory>
 
-static unsigned char *load_data(FILE *fp, size_t ofst, size_t sz);
-static unsigned char *load_model(const char *filename, int *model_size);
-
-class rknn_lite
+// rknnModel模型类, inputType模型输入类型, outputType模型输出类型
+// template <typename rknnModel, typename inputType, typename outputType>
+class rknnPool
 {
 private:
-    rknn_context rkModel;
-    unsigned char *model_data;
-    rknn_sdk_version version;
-    rknn_input_output_num io_num;
-    rknn_tensor_attr *input_attrs;
-    rknn_tensor_attr *output_attrs;
-    rknn_input inputs[1];
-    int ret;
-    int channel = 3;
-    int width = 0;
-    int height = 0;
-    float findMode(const std::map<float, int> &depth_counts);
-    float depthImageRecv(const sensor_msgs::msg::Image &depth_image_msg, int top, int bottom, int left, int right);
-    float getDepthAtPixel(const sensor_msgs::msg::Image &depth_image_msg, int x, int y);
+    int threadNum;
+    std::string modelPath;
+
+    long long id;
+    std::mutex idMtx, queueMtx;
+    std::unique_ptr<dpool::ThreadPool> pool;
+    std::queue<std::future<cv::Mat>> futs;
+    std::vector<std::shared_ptr<rkYolov5s>> models;
+
+protected:
+    int getModelId();
 
 public:
-    Mat ori_img;
-    sensor_msgs::msg::Image depth_image_msg_ptr;
-    msg_interfaces::msg::RGBCameraObstacle rgb_frames_data;
-    int interf();
-    rknn_lite(char *dst, int n);
-    ~rknn_lite();
+    rknnPool(const std::string modelPath, int threadNum);
+    int init();
+    // 模型推理/Model inference
+    int put(cv::Mat inputData);
+    // 获取推理结果/Get the results of your inference
+    int get(cv::Mat &outputData);
+    ~rknnPool();
 };
-float rknn_lite::findMode(const std::map<float, int> &depth_counts)
+
+rknnPool::rknnPool(const std::string modelPath, int threadNum)
 {
-    // 找到映射中出现次数最多的深度值
-    auto mode_iterator = std::max_element(
-        depth_counts.begin(), depth_counts.end(),
-        [](const auto &a, const auto &b)
-        { return a.second < b.second; });
-    // 返回众数深度值
-    return mode_iterator->first;
+    this->modelPath = modelPath;
+    this->threadNum = threadNum;
+    this->id = 0;
 }
 
-float rknn_lite::getDepthAtPixel(const sensor_msgs::msg::Image &depth_image_msg, int x, int y)
+int rknnPool::init()
 {
     try
     {
-        // 获取深度图像的宽度和高度
-        int width = depth_image_msg.width;
-        int height = depth_image_msg.height;
-
-        // 检查像素位置是否在图像范围内
-        if (x < 0 || x >= width || y < 0 || y >= height)
-        {
-            // 处理越界情况，这里可以抛出异常或返回默认值
-            return 0.0f; // 默认深度值为0.0
-        }
-
-        // 假设深度图像数据类型是单通道16位无符号整数，单位是毫米
-        const uint16_t *depth_data = reinterpret_cast<const uint16_t *>(depth_image_msg.data.data());
-
-        // 计算深度图像中的索引
-        int index = y * width + x;
-
-        // 检查数组长度是否足够
-        if (index >= depth_image_msg.data.size() / sizeof(uint16_t))
-        {
-            // 处理数组越界情况，这里可以抛出异常或返回默认值
-            return 0.0f; // 默认深度值为0.0
-        }
-
-        // 获取深度值
-        uint16_t depth_value = depth_data[index];
-
-        // 将深度值转换为浮点数并返回
-        return static_cast<float>(depth_value);
+        this->pool = std::make_unique<dpool::ThreadPool>(this->threadNum);
+        for (int i = 0; i < this->threadNum; i++)
+            models.push_back(std::make_shared<rkYolov5s>(this->modelPath.c_str()));
     }
-    catch (const std::exception &e)
+    catch (const std::bad_alloc &e)
     {
-        std::cerr << e.what() << '\n';
+        std::cout << "Out of memory: " << e.what() << std::endl;
+        return -1;
     }
-}
-
-float rknn_lite::depthImageRecv(const sensor_msgs::msg::Image &depth_image_msg, int top, int bottom, int left, int right)
-{
-    std::map<float, int> depth_counts;
-
-    for (int y = top; y <= bottom; ++y)
+    // 初始化模型/Initialize the model
+    for (int i = 0, ret = 0; i < threadNum; i++)
     {
-        for (int x = left; x <= right; ++x)
-        {
-            // 获取深度值
-            float depth = getDepthAtPixel(depth_image_msg, x, y);
-
-            if (depth > 0)
-            {
-                // 将深度值添加到映射中
-                depth_counts[depth]++;
-            }
-        }
+        ret = models[i]->init(models[0]->get_pctx(), i != 0);
+        if (ret != 0)
+            return ret;
     }
 
-    float mode_depth = findMode(depth_counts);
-    return mode_depth;
-}
-
-rknn_lite::rknn_lite(char *model_name, int n)
-{
-    /* Create the neural network */
-    printf("Loading mode...\n");
-    int model_data_size = 0;
-    // 读取模型文件数据
-    model_data = load_model(model_name, &model_data_size);
-    // 通过模型文件初始化rknn类
-    ret = rknn_init(&rkModel, model_data, model_data_size, 0, NULL);
-    if (ret < 0)
-    {
-        printf("rknn_init error ret=%d\n", ret);
-        exit(-1);
-    }
-    //
-    rknn_core_mask core_mask;
-    if (n == 0)
-        core_mask = RKNN_NPU_CORE_0;
-    else if (n == 1)
-        core_mask = RKNN_NPU_CORE_1;
-    else
-        core_mask = RKNN_NPU_CORE_2;
-    int ret = rknn_set_core_mask(rkModel, core_mask);
-    if (ret < 0)
-    {
-        printf("rknn_init core error ret=%d\n", ret);
-        exit(-1);
-    }
-
-    // 初始化rknn类的版本
-    ret = rknn_query(rkModel, RKNN_QUERY_SDK_VERSION, &version, sizeof(rknn_sdk_version));
-    if (ret < 0)
-    {
-        printf("rknn_init error ret=%d\n", ret);
-        exit(-1);
-    }
-
-    // 获取模型的输入参数
-    ret = rknn_query(rkModel, RKNN_QUERY_IN_OUT_NUM, &io_num, sizeof(io_num));
-    if (ret < 0)
-    {
-        printf("rknn_init error ret=%d\n", ret);
-        exit(-1);
-    }
-
-    // 设置输入数组
-    input_attrs = new rknn_tensor_attr[io_num.n_input];
-    memset(input_attrs, 0, sizeof(input_attrs));
-    for (int i = 0; i < io_num.n_input; i++)
-    {
-        input_attrs[i].index = i;
-        ret = rknn_query(rkModel, RKNN_QUERY_INPUT_ATTR, &(input_attrs[i]), sizeof(rknn_tensor_attr));
-        if (ret < 0)
-        {
-            printf("rknn_init error ret=%d\n", ret);
-            exit(-1);
-        }
-    }
-
-    // 设置输出数组
-    output_attrs = new rknn_tensor_attr[io_num.n_output];
-    memset(output_attrs, 0, sizeof(output_attrs));
-    for (int i = 0; i < io_num.n_output; i++)
-    {
-        output_attrs[i].index = i;
-        ret = rknn_query(rkModel, RKNN_QUERY_OUTPUT_ATTR, &(output_attrs[i]), sizeof(rknn_tensor_attr));
-    }
-
-    // 设置输入参数
-    if (input_attrs[0].fmt == RKNN_TENSOR_NCHW)
-    {
-        printf("model is NCHW input fmt\n");
-        channel = input_attrs[0].dims[1];
-        height = input_attrs[0].dims[2];
-        width = input_attrs[0].dims[3];
-    }
-    else
-    {
-        printf("model is NHWC input fmt\n");
-        height = input_attrs[0].dims[1];
-        width = input_attrs[0].dims[2];
-        channel = input_attrs[0].dims[3];
-    }
-
-    memset(inputs, 0, sizeof(inputs));
-    inputs[0].index = 0;
-    inputs[0].type = RKNN_TENSOR_UINT8;
-    inputs[0].size = width * height * channel;
-    inputs[0].fmt = RKNN_TENSOR_NHWC;
-    inputs[0].pass_through = 0;
-}
-
-rknn_lite::~rknn_lite()
-{
-    ret = rknn_destroy(rkModel);
-    delete[] input_attrs;
-    delete[] output_attrs;
-    if (model_data)
-        free(model_data);
-}
-
-int rknn_lite::interf()
-{
-    cv::Mat img;
-    // 获取图像宽高
-    int img_width = ori_img.cols;
-    int img_height = ori_img.rows;
-    cv::cvtColor(ori_img, img, cv::COLOR_BGR2RGB);
-
-    // init rga context
-    // rga是rk自家的绘图库,绘图效率高于OpenCV
-    rga_buffer_t src;
-    rga_buffer_t dst;
-    memset(&src, 0, sizeof(src));
-    memset(&dst, 0, sizeof(dst));
-    im_rect src_rect;
-    im_rect dst_rect;
-    memset(&src_rect, 0, sizeof(src_rect));
-    memset(&dst_rect, 0, sizeof(dst_rect));
-
-    // You may not need resize when src resulotion equals to dst resulotion
-    void *resize_buf = nullptr;
-    // 如果输入图像不是指定格式
-    if (img_width != width || img_height != height)
-    {
-        resize_buf = malloc(height * width * channel);
-        memset(resize_buf, 0x00, height * width * channel);
-
-        src = wrapbuffer_virtualaddr((void *)img.data, img_width, img_height, RK_FORMAT_RGB_888);
-        dst = wrapbuffer_virtualaddr((void *)resize_buf, width, height, RK_FORMAT_RGB_888);
-        ret = imcheck(src, dst, src_rect, dst_rect);
-        if (IM_STATUS_NOERROR != ret)
-        {
-            printf("%d, check error! %s", __LINE__, imStrError((IM_STATUS)ret));
-            exit(-1);
-        }
-        IM_STATUS STATUS = imresize(src, dst);
-
-        cv::Mat resize_img(cv::Size(width, height), CV_8UC3, resize_buf);
-        inputs[0].buf = resize_buf;
-    }
-    else
-        inputs[0].buf = (void *)img.data;
-
-    // 设置rknn的输入数据
-    rknn_inputs_set(rkModel, io_num.n_input, inputs);
-
-    // 设置输出
-    rknn_output outputs[io_num.n_output];
-    memset(outputs, 0, sizeof(outputs));
-    for (int i = 0; i < io_num.n_output; i++)
-        outputs[i].want_float = 0;
-    // 调用npu进行推演
-    ret = rknn_run(rkModel, NULL);
-    // 获取npu的推演输出结果
-    ret = rknn_outputs_get(rkModel, io_num.n_output, outputs, NULL);
-
-    // 总之就是绘图部分
-    // post process
-    // width是模型需要的输入宽度, img_width是图片的实际宽度
-    const float nms_threshold = NMS_THRESH;
-    const float box_conf_threshold = BOX_THRESH;
-    float scale_w = (float)width / img_width;
-    float scale_h = (float)height / img_height;
-
-    detect_result_group_t detect_result_group;
-    std::vector<float> out_scales;
-    std::vector<int32_t> out_zps;
-    for (int i = 0; i < io_num.n_output; ++i)
-    {
-        out_scales.push_back(output_attrs[i].scale);
-        out_zps.push_back(output_attrs[i].zp);
-    }
-    post_process((int8_t *)outputs[0].buf, (int8_t *)outputs[1].buf, (int8_t *)outputs[2].buf, height, width,
-                 box_conf_threshold, nms_threshold, scale_w, scale_h, out_zps, out_scales, &detect_result_group);
-
-    // Draw Objects
-    char text[256];
-    for (int i = 0; i < detect_result_group.count; i++)
-    {
-        detect_result_t *det_result = &(detect_result_group.results[i]);
-        int x1 = det_result->box.left;
-        int y1 = det_result->box.top;
-        int x2 = det_result->box.right;
-        int y2 = det_result->box.bottom;
-        double deep = depthImageRecv(depth_image_msg_ptr, y1, y2, x1, x2);
-        ;
-        int w = x2 - x1;
-        int h = y2 - y1;
-        sprintf(text, "%s %.1f%% depth: %.1f", det_result->name, det_result->prop * 100, deep / 1000);
-        putText(ori_img, text, cv::Point(x1, y1 + 12), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
-        rectangle(ori_img, cv::Point(x1, y1), cv::Point(det_result->box.right, det_result->box.bottom), cv::Scalar(0, 0, 255, 0), 3);
-        // ROS output
-        rgb_frames_data.obstacle_type.push_back(det_result->name);
-        rgb_frames_data.obstacle_position.push_back(x1);
-        rgb_frames_data.obstacle_position.push_back(y1);
-        rgb_frames_data.obstacle_position.push_back(x2);
-        rgb_frames_data.obstacle_position.push_back(y2);
-        // 640 to 1280
-        rgb_frames_data.obstacle_size.push_back(deep);
-    }
-    ret = rknn_outputs_release(rkModel, io_num.n_output, outputs);
-    if (resize_buf)
-    {
-        free(resize_buf);
-    }
     return 0;
 }
 
-static unsigned char *load_data(FILE *fp, size_t ofst, size_t sz)
+int rknnPool::getModelId()
 {
-    unsigned char *data;
-    int ret;
-
-    data = NULL;
-
-    if (NULL == fp)
-    {
-        return NULL;
-    }
-
-    ret = fseek(fp, ofst, SEEK_SET);
-    if (ret != 0)
-    {
-        printf("blob seek failure.\n");
-        return NULL;
-    }
-
-    data = (unsigned char *)malloc(sz);
-    if (data == NULL)
-    {
-        printf("buffer malloc failure.\n");
-        return NULL;
-    }
-    ret = fread(data, 1, sz, fp);
-    return data;
+    std::lock_guard<std::mutex> lock(idMtx);
+    int modelId = id % threadNum;
+    id++;
+    return modelId;
 }
 
-static unsigned char *load_model(const char *filename, int *model_size)
+int rknnPool::put(cv::Mat inputData)
 {
-    FILE *fp;
-    unsigned char *data;
+    futs.push(pool->submit(&rkYolov5s::infer, models[this->getModelId()], inputData));
+    return 0;
+}
 
-    fp = fopen(filename, "rb");
-    if (NULL == fp)
+int rknnPool::get(cv::Mat &outputData)
+{
+    std::lock_guard<std::mutex> lock(queueMtx);
+    if (futs.empty() == true)
+        return 1;
+    outputData = futs.front().get();
+    futs.pop();
+    return 0;
+}
+
+rknnPool::~rknnPool()
+{
+    while (!futs.empty())
     {
-        printf("Open file %s failed.\n", filename);
-        return NULL;
+        cv::Mat temp = futs.front().get();
+        futs.pop();
     }
-
-    fseek(fp, 0, SEEK_END);
-    int size = ftell(fp);
-
-    data = load_data(fp, 0, size);
-
-    fclose(fp);
-
-    *model_size = size;
-    return data;
 }
 
 #endif
--- rknn_2/src/main.cc
+++ rknn_2/src/main.cc
@@ -1,173 +1,101 @@
-// Copyright (c) 2021 by Rockchip Electronics Co., Ltd. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/*-------------------------------------------
-                Includes
--------------------------------------------*/
-
-// ROS2 Node
-
 #include <stdio.h>
+#include <memory>
 #include <sys/time.h>
-#include <thread>
-#include <queue>
-#include <vector>
-#include <iostream>
-#include <filesystem>
-#include <algorithm>
-#define _BASETSD_H
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+
+#include "rknnPool.hpp"
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/highgui/highgui.hpp>
-#include "rknnPool.hpp"
-#include "ThreadPool.hpp"
-#include "postprocess.h"
-#include <msg_interfaces/msg/rgb_camera_obstacle.hpp>
-#define MODEL_PATH "/home/ums/yolov5_md/model/RK3588/yolov5s.rknn"
-
-using std::queue;
-using std::time;
-using std::time_t;
-using std::vector;
 
 class ImageSubscriberNode : public rclcpp::Node
 {
 public:
-  ImageSubscriberNode() : Node("image_subscriber"), tdpool(6)
-  {
-    // 订阅 /color/raw 话题，回调函数为 processImage
-    subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-        "/camera/color/image_raw", rclcpp::QoS(5).best_effort(), std::bind(&ImageSubscriberNode::imageCallback, this, std::placeholders::_1));
-    image_publisher_ = create_publisher<sensor_msgs::msg::Image>("image_topic", rclcpp::QoS(1).best_effort());
-    rgb_obstacle_publisher_ = create_publisher<msg_interfaces::msg::RGBCameraObstacle>("rgb_camera/obstacle", rclcpp::QoS(5).best_effort());
+    ImageSubscriberNode() : Node("image_subscriber")
+    {
+        int init = poolPtr.init();
+        if (init != 0)
+        {
+            RCLCPP_ERROR(this->get_logger(), "rknn Pool init fail");
+        }
+        // 订阅 /color/raw 话题，回调函数为 processImage
+        subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+            "/camera/color/image_raw", rclcpp::QoS(5).best_effort(), std::bind(&ImageSubscriberNode::imageCallback, this, std::placeholders::_1));
+        image_publisher_ = create_publisher<sensor_msgs::msg::Image>("image_topic", rclcpp::QoS(1).best_effort());
+        // 订阅深度图像和相机信息
+        depth_image_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+            "/camera/depth/image_rect_raw", rclcpp::QoS(5).best_effort(),
+            std::bind(&ImageSubscriberNode::depthImageCallback, this, std::placeholders::_1));
 
-    // 订阅深度图像和相机信息
-    depth_image_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-        "/camera/depth/image_rect_raw", rclcpp::QoS(5).best_effort(),
-        std::bind(&ImageSubscriberNode::depthImageCallback, this, std::placeholders::_1));
-
-    camera_info_subscription_ = this->create_subscription<sensor_msgs::msg::CameraInfo>(
-        "/camera/depth/camera_info", 10,
-        std::bind(&ImageSubscriberNode::cameraInfoCallback, this, std::placeholders::_1));
-    // Create a MultiThreadedExecutor with the specified number of threads
-    printf("模型名称:\t%s\n", MODEL_PATH);
-  }
+        camera_info_subscription_ = this->create_subscription<sensor_msgs::msg::CameraInfo>(
+            "/camera/depth/camera_info", 10,
+            std::bind(&ImageSubscriberNode::cameraInfoCallback, this, std::placeholders::_1));
+        RCLCPP_INFO(this->get_logger(), "rknn node start");
+    }
 
 private:
-  void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg)
-  {
-    try
+    void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg)
     {
-      // Convert ROS Image message to OpenCV image
-      cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
-      // Process the image (e.g., perform inference)
-      processImage(cv_ptr->image);
+        try
+        {
+            // Convert ROS Image message to OpenCV image
+            cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
+            // Process the image (e.g., perform inference)
+            processImage(cv_ptr->image);
+        }
+        catch (const std::exception &e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "cv exception: %s", e.what());
+        }
     }
-    catch (const std::exception &e)
+    void processImage(const cv::Mat &image)
     {
-      RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
-    }
-  }
-  void processImage(const cv::Mat &image)
-  {
-    try
-    {
-      mat_ptr = cv::Mat(image);
-      initRkPool();
-      if (count > n)
-      {
-        // 发布帧
-        futs.front().get();
-        futs.pop();
-        cv::Mat cvImage = rkpool[frames % n]->ori_img;
-        // cv::imshow("Camera FPS", cvImage);
+
+        poolPtr.put(image);
+        cv::Mat modifiableImage = image; // Create a modifiable copy
+
+        if (count > 3)
+        {
+            poolPtr.get(modifiableImage);
+        }
+        else
+        {
+            count++;
+        }
+
         sensor_msgs::msg::Image::SharedPtr rosImage = std::make_shared<sensor_msgs::msg::Image>();
-        cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", cvImage).toImageMsg(*rosImage);
-        rgb_obstacle_publisher_->publish(rkpool[frames % n]->rgb_frames_data);
+        cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", modifiableImage).toImageMsg(*rosImage);
         image_publisher_->publish(*rosImage);
-        // 更新帧
-        rkpool[frames % n]->ori_img = cv::Mat(mat_ptr);
-        sensor_msgs::msg::Image depth_image_copy;
-        depth_image_copy.height = depth_image_msg_gb->height;
-        depth_image_copy.width = depth_image_msg_gb->width;
-        depth_image_copy.data = depth_image_msg_gb->data;
-
-        // Assign the copied object to depth_image_msg_ptr
-        rkpool[frames % n]->depth_image_msg_ptr = depth_image_copy;
-        msg_interfaces::msg::RGBCameraObstacle clean;
-        rkpool[frames % n]->rgb_frames_data = clean;
-        futs.push(tdpool.submit(&rknn_lite::interf, &(*rkpool[frames++ % n])));
-      }
-      else
-      {
-        count++;
-      }
     }
-    catch (const std::exception &e)
+
+    void depthImageCallback(const sensor_msgs::msg::Image::SharedPtr depth_image_msg)
     {
-      RCLCPP_ERROR(this->get_logger(), "yolo exception: %s", e.what());
+        depth_image_msg_gb = depth_image_msg;
     }
-  }
 
-  void initRkPool()
-  {
-    if (count < n)
-    { 
-      char* model_path_char = MODEL_PATH;
-      auto rknn_model = std::make_shared<rknn_lite>(model_path_char, count % 3);
-      rkpool.push_back(rknn_model);
-      rknn_model->ori_img = cv::Mat(mat_ptr); // Assuming mat_ptr is available
-      futs.push(tdpool.submit(&rknn_lite::interf, rknn_model.get()));
+    void cameraInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr camera_info_msg)
+    {
+        // 在这里处理相机信息，可以使用camera_info_msg等
+        // 获取相机的内参矩阵等信息，用于坐标变换
+        // 这里假设你已经实现了获取相机内参矩阵的函数 getCameraMatrix(camera_info_msg)，
+        // 具体实现方式可以参考 sensor_msgs::msg::CameraInfo 文档。
     }
-  }
-  void depthImageCallback(const sensor_msgs::msg::Image::SharedPtr depth_image_msg)
-  {
-    depth_image_msg_gb = depth_image_msg;
-  }
-
-  void cameraInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr camera_info_msg)
-  {
-    // 在这里处理相机信息，可以使用camera_info_msg等
-    // 获取相机的内参矩阵等信息，用于坐标变换
-    // 这里假设你已经实现了获取相机内参矩阵的函数 getCameraMatrix(camera_info_msg)，
-    // 具体实现方式可以参考 sensor_msgs::msg::CameraInfo 文档。
-  }
-  cv::Mat mat_ptr;
-  cv_bridge::CvImageConstPtr cv_ptr;
-  sensor_msgs::msg::Image::SharedPtr depth_image_msg_gb;
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_publisher_;
-  rclcpp::Publisher<msg_interfaces::msg::RGBCameraObstacle>::SharedPtr rgb_obstacle_publisher_;
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_image_subscription_;
-  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_subscription_;
-  int n = 6, frames = 0; // 设置线程数
-  int count = 0;
-  std::vector<std::shared_ptr<rknn_lite>> rkpool;
-  std::queue<std::future<int>> futs;
-  dpool::ThreadPool tdpool;
+    int count = 0;
+    int frames = 0;
+    cv_bridge::CvImageConstPtr cv_ptr;
+    sensor_msgs::msg::Image::SharedPtr depth_image_msg_gb;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_publisher_;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_image_subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_subscription_;
+    rknnPool poolPtr = rknnPool("/home/umsrock5a/model/yolov5.rknn", 3);
 };
 
 int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<ImageSubscriberNode>());
-  rclcpp::shutdown();
-  return 0;
-}
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<ImageSubscriberNode>());
+    rclcpp::shutdown();
+    return 0;
+};
